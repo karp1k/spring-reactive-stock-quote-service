72a7188f3465a146b792eaf583b83bf517c119d8
Tom <desktop-vm-ubuntu>
2020-06-08 12:55:55 +0300

    Add Router for Handler

---
+++ src/main/java/guru/springframework/springreactivestockquoteservice/web/QuoteRouter.java
@@ -0,0 +1,21 @@
+package guru.springframework.springreactivestockquoteservice.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
+
+
+/**
+ * @author kas
+ */
+@Configuration
+public class QuoteRouter {
+
+    @Bean
+    RouterFunction<ServerResponse> route(QuoteHandler quoteHandler) {
+        return RouterFunctions.route(
+                RequestPredicates.GET("/quote").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+                quoteHandler::fetchQoutes);
+    }
+}
